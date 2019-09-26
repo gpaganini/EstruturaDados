46684e91d078687099c70f906391a9dc02f21b18
Giovani Paganini <giovanipaganini@outlook.com>
2019-09-26 20:14:48 -0300

    PILHA01

---
+++ Pilha01/src/com/paganini/ed/PilhaAR.java
@@ -0,0 +1,72 @@
+package com.paganini.ed;
+
+import com.sun.codemodel.internal.JOp;
+
+import javax.swing.*;
+
+public class PilhaAR {
+    int[] pilha = new int[10];
+    int topoPilha = -1;
+
+    void esvazie_pilha() {
+        this.topoPilha = -1;
+    }
+
+    int tamanho(){
+        return(this.topoPilha + 1);
+    }
+
+    void imprimir(int topo) {
+        for(int i = topo; i >= 0; i--) {
+            System.out.println("Valor na pilha: " +this.pilha);
+        }
+    }
+
+    int push(int topo, int num) {
+        this.topoPilha = topo;
+        this.topoPilha++;
+
+        if (this.topoPilha < 0) {
+            this.topoPilha = 0;
+        }
+
+        if (this.topoPilha > 9) {
+            JOptionPane.showMessageDialog(null,
+                    "STACK OVERFLOW, ERROR: -999");
+            return(this.topoPilha--);
+        } else {
+            this.pilha[this.topoPilha] = num;
+            System.out.println("Inserindo o elemento " + num);
+            return(this.topoPilha);
+        }
+    }
+
+    int pop(int topo) {
+        this.topoPilha = topo;
+        this.topoPilha--;
+
+        if (this.topoPilha < -1) {
+            JOptionPane.showMessageDialog(null,
+                    "Pilha vazia. Erro -997");
+            return (-997);
+        } else {
+            System.out.println("Retirando o valor: "
+                    +this.pilha[this.topoPilha + 1]);
+            return (this.pilha[this.topoPilha+ 1]);
+        }
+
+    }
+
+    int top(int topo){
+        this.topoPilha = topo;
+
+        if (this.topoPilha < 0) {
+            JOptionPane.showMessageDialog(null,
+                    "Pilha vazia. Erro -998");
+            return(-998);
+        } else {
+            return (this.pilha[this.topoPilha]);
+        }
+    }
+
+}
