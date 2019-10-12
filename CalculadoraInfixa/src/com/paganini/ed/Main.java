da5256c317500fa2bae4dfa16a2755177f5dceb5
Giovani Paganini <giovanipaganini@outlook.com>
2019-10-12 19:48:18 -0300

    Main class changes

--- CalculadoraInfixa/src/com/paganini/ed/Main.java
+++ CalculadoraInfixa/src/com/paganini/ed/Main.java
@@ -1,15 +1,36 @@
 package com.paganini.ed;
 
+import javax.swing.*;
+
 public class Main {
 
     public static void main(String[] args) {
-        ValidaPosfixa vp = new ValidaPosfixa();
-
+        ValidaPosfixa valPos = new ValidaPosfixa();
+        ValidaPrefixa valPre = new ValidaPrefixa();
         String exp = "(2+3)*5";
-        System.out.println(exp);
+
+        String expressao = JOptionPane.showInputDialog("Digite uma operação no formato infixo: ");
+        System.out.println("Expressão infixa digitada: " +expressao);
+        System.out.println("Infixo para pós-fixo: " +valPos.infixoParaPosfixo(expressao));
+        System.out.println("Resultado infixo para pós-fixo: " +valPos.calcular(expressao));
+        System.out.println("Pós-fixo para Pré-fixo: " +valPre.posfixoParaPrefixo(valPos.infixoParaPosfixo(expressao)));
+        System.out.println("Resultado pós-fixo para pré-fixo: " +valPre.calcular(valPos.infixoParaPosfixo(expressao)));
+
+
+
+        /*String prefix = valPre.posfixoParaPrefixo(valPos.infixoParaPosfixo(exp));
+        System.out.println("pos fixo para prefixo: " +prefix);
+
+        System.out.println(valPre.evaluatePrefix(prefix));
+
+        System.out.println("Calcular: " );*/
+
+       // System.out.println("Posfixo2Prefixo validado: " +valPre.evaluatePrefix(post2pre));
+
+
+
         String exp2 = "231*+9-";
-        System.out.println("Conversão infixa para pós fixa: " +vp.infixoParaPosfixo(exp));
-        System.out.println("Validação Pós Fixa: " +vp.evaluatePostfix(vp.infixoParaPosfixo(exp)));
+
 
         /*String post = vp.infixoParaPosfixo(exp);
         System.out.println(vp.infixoParaPosfixo(exp));
