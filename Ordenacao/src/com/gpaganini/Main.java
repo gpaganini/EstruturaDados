2586e0f62be39d09fd545ed2a7916ee5be03192d
Giovani Paganini <giovanipaganini@outlook.com>
2019-11-28 13:41:36 -0300

    Correção da geração dos vetores desordenadas

--- Ordenacao/src/com/gpaganini/Main.java
+++ Ordenacao/src/com/gpaganini/Main.java
@@ -19,39 +19,36 @@ public class Main {
         PrintWriter gravaArq = new PrintWriter(arq);
 
         gravaArq.println("===Desordenado===\n");
-
         for (int i = 0; i < 1000000; i++) {
-            gravaArq.println(vet[i] = r.nextInt(10000));
+            vet[i] = r.nextInt(10000);
         }
+        gravaArq.println(Arrays.toString(vet));
 
         gravaArq.println("\n===Ordenado com QuickSort===\n");
-
         long start1 = System.currentTimeMillis();
         ord.quicksort(vet, 0, vet.length - 1);
         gravaArq.println(Arrays.toString(vet));
         long finish1 = System.currentTimeMillis();
         long elapsed1 = finish1 - start1;
-        gravaArq.println("\nTempo de execução: " +dateFormat.format(new Date(elapsed1)));
 
         gravaArq.println("\n===Ordenado com BubbleSort===\n");
-
         long start2 = System.currentTimeMillis();
         ord.bubbleSort(vet);
         gravaArq.println(Arrays.toString(vet));
         long finish2 = System.currentTimeMillis();
         long elapsed2 = finish2 - start2;
-        gravaArq.println("\nTempo de execução: " +dateFormat.format(new Date(elapsed2)));
 
         gravaArq.println("\n===Ordenado com SelectionSort===\n");
-
         long start3 = System.currentTimeMillis();
         ord.selectionSort(vet);
         gravaArq.println(Arrays.toString(vet));
         long finish3 = System.currentTimeMillis();
         long elapsed3 = finish3 - start3;
-        gravaArq.println("\nTempo de execução: " +dateFormat.format(new Date(elapsed3)));
-
         long totalElapsed = finish3 - start1;
+
+        gravaArq.println("\nTempo de execução QuickSort: " +dateFormat.format(new Date(elapsed1)));
+        gravaArq.println("\nTempo de execução BubbleSort: " +dateFormat.format(new Date(elapsed2)));
+        gravaArq.println("\nTempo de execução SelectionSort: " +dateFormat.format(new Date(elapsed3)));
         gravaArq.println("\nTempo Total de execução: " +dateFormat.format(new Date(totalElapsed)));
 
         arq.close();
